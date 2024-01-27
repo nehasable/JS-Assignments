ce0677b006c5a972052c777523ae3cc92eb54b15
nehasable <nehasable45@gmail.com>
2024-01-27 23:42:16 +0530

    commit

---
+++ index.js
@@ -0,0 +1,25 @@
+const express=require("express")
+const app=express()
+
+const allUsers=[{
+username:"nehasable45@xyz",
+password:"Neha",
+name:"Neha"
+
+},
+{
+    username:"sable45@xyz",
+    password:"ha",
+    name:"ha"   
+}]
+
+
+
+
+app.post("/signin" ,function(req,resp){
+    return
+} )
+
+app.get("/users",function(req,resp){
+    
+})
