acc4b39e1051df54425a40709471b31f608bcfb8
nehasable <nehasable45@gmail.com>
2024-01-27 12:44:56 +0530

    commit

--- express.js
+++ express.js
@@ -7,7 +7,38 @@ kidneys:[{
     healthy:"false"
 }]
 }]
+//function for authenticating user and kidney
+//here next() is a callback method that passes the processing to the next function is line and it doesnt contain any response
+function userMiddleware(req, resp, next) {
+    const username = req.query.username;
+    const password = req.query.password;
 
+    if (username !== "neha" || password !== "poo") { 
+        console.log("Access Denied");
+        resp.status(403).json({
+            msg: "wrong person!"
+        });
+    } else {
+        console.log("Access Granted");
+        next();
+    }
+}
+
+function kidneyMiddleware(req, resp, next) {
+    const kidneyId = req.query.kidneyId;
+    
+    if (kidneyId != 1 && kidneyId != 2) {
+        console.log("Access Denied");
+        resp.status(411).json({
+            msg: "false!"
+        });
+    } else {
+        console.log("Access Granted");
+        next();
+    }
+}
+
+//-------------------calling user and its kidneys---------------------
 app.get("/",function(req,resp ){
     //we want to know whether the first user's kidney is healthy or no
 const userKidneys=users[0].kidneys
@@ -28,27 +59,16 @@ resp.json({
     numOfHealthyKidneys:numOfHealthyKidneys.length
 })
 })
+//--------------------authenticating user and its kidneys(here data is obtained from db)----
 
-app.get('/kidney',function(req,resp){
-    const kidneyId=req.query.kidneyId
-    const username=req.headers.username
-    const password=req.headers.password
 
-   if (username!="neha" || password!="poo") {     //or because either one of those have to be wrong to invoke error
-    resp.status(403).json({
-        msg:"wrong person!"
-    })
-    return;
-   }
-
-   if (kidneyId !=1 && kidneyId !=2) {       //and because both 1 and 2 are valid ans
-    resp.status(411).json({
-        msg:"false!"
-    })
-    return;
-   }
-   
-   resp.send("healthy kidney")
+app.get('/newhealth',userMiddleware,kidneyMiddleware, function(req,resp){
+    resp.send("healthy kidney")
 })
 
+app.post('/kidneyinfo',function(req,resp){
+    const kidneys=req.body.kidneys
+    const kidneyLength=kidneys.length()
+    resp.send("you have" +kidneyLength)
+})
 app.listen(3000)
