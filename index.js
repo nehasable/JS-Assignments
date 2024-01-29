05867910d0a8809ae208015b8b570aca915e782e
nehasable <nehasable45@gmail.com>
2024-01-29 19:02:17 +0530

    commit

--- index.js
+++ index.js
@@ -1,25 +1,45 @@
-const express=require("express")
-const app=express()
+const express = require("express");
+const jwt = require('jsonwebtoken');
+const jwtPass = "12345";
 
-const allUsers=[{
-username:"nehasable45@xyz",
-password:"Neha",
-name:"Neha"
+const app = express();
+app.use(express.json())
+const allUsers = [
+  {
+    username: "nehasable45@xyz",
+    password: "Neha",
+    name: "Neha"
+  },
+  {
+    username: "sable45@xyz",
+    password: "ha",
+    name: "ha"
+  }
+];
 
-},
-{
-    username:"sable45@xyz",
-    password:"ha",
-    name:"ha"   
-}]
+app.post("/signin", function(req, resp) {
+  const username = req.body.username;
+  const password = req.body.password;
 
+  // Check if the user exists
+  const validUser = allUsers.find(user => user.username === username && user.password === password);
 
+  if (!validUser) {
+    return resp.status(403).json({
+      msg: "User doesn't exist or invalid credentials"
+    });
+  }
 
+  // User is valid, generate a token
+  const token = jwt.sign({ username }, jwtPass);
+  resp.json({ token });
+});
 
-app.post("/signin" ,function(req,resp){
-    return
-} )
+app.get("/users", function(req, resp) {
+  // Placeholder for "/users" route
+  resp.send("List of users");
+});
 
-app.get("/users",function(req,resp){
-    
-})
+app.listen(3000, () => {
+  console.log("Server connected on port 3000");
+});
